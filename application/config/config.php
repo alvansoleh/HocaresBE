f181cf46e72a76e1e7f9c7c8520a807e7ae42fdf
alvansoleh <134778608+alvansoleh@users.noreply.github.com>
2024-05-12 21:48:30 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://startide.id/project/sim_rs/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
